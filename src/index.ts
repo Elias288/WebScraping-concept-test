e5c68208c035ef134a68156ec5e53c83df89f6c0
Elias <bianchi.elias@gmail.com>
2025-07-19 22:04:31 -0300

    feat: List of urls

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-import * as cheerio from 'cheerio'
-import ejs from 'ejs'
-import fs from 'fs'
+import * as cheerio from "cheerio";
+import ejs from "ejs";
+import fs from "fs";
 
-const TEMPLATE_PATH ='./views/template.ejs' 
-const OUT_HTML_FILE = "./dist/index.html"
-const URL = "https://www.bitgree.com/earn-bitcoin-cash?fc%5B0%5D=us";
+const TEMPLATE_PATH = "./views/template.ejs";
+const OUT_HTML_FILE = "./dist/index.html";
+const URL = "https://www.bitgree.com/earn-bitcoin-cash?fc%5B0%5D=<id>";
+const URLS = [
+  { country: "EEUU", id: "us" },
+  { country: "Uruguay", id: "uy" },
+  { country: "Argentina", id: "ar" },
+];
 
 interface Product {
   title: string;
   image: string;
   price: string;
   date: string;
+  country: string;
 }
 
-async function fetchData(): Promise<Product[]> {
-  const res = await fetch(URL);
+async function fetchData(country: string, url: string): Promise<Product[]> {
+  const res = await fetch(url);
   const page = await res.text();
   const $ = cheerio.load(page);
   const productsCards = $(".product-card");
@@ -26,30 +32,42 @@ async function fetchData(): Promise<Product[]> {
       const price = $(el).find(".product-price-fiat").text().trim();
       const date = $(el).find(".wish-waiting-time").text().trim();
 
-      return { title, image, price, date };
+      return { title, image, price, date, country };
     })
     .get();
 
   return products;
 }
 
-function loadData(): void {
-  const posts = fetchData()
-  posts.then(posts => {
-    const data = {
-      items: posts
+async function loadData(): Promise<void> {
+  const posts = await Promise.all(
+    URLS.map((u) => {
+      return fetchData(u.country, URL.replace("<id>", u.id));
+    })
+  );
+  const data = {
+    items: posts.flat(),
+    creationDate: new Date().toLocaleDateString(),
+  };
+
+  ejs.renderFile(TEMPLATE_PATH, data, (err, html) => {
+    if (err) {
+      console.error("Error al renderizar: ", err);
+      return;
     }
 
-    ejs.renderFile(TEMPLATE_PATH, data, (err, html) => {
-      if(err) {
-        console.error("Error al renderizar: ", err);
-        return
-      }
-
-      fs.writeFileSync(OUT_HTML_FILE, html)
-    })
-  })
+    fs.writeFileSync(OUT_HTML_FILE, html);
+  });
 }
 
-loadData()
-// fetchData().then(d => console.log(d));
+async function showList() {
+  const res = await Promise.all(
+    URLS.map((u) => {
+      return fetchData(u.country, URL.replace("<id>", u.id));
+    })
+  );
+  console.log(res.flat());
+}
+
+loadData();
+// showList();
